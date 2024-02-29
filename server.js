83c6f93ac814387639acae056f5e22084c0f7869
sbraviprakasha <149754475+sbraviprakasha@users.noreply.github.com>
2024-02-29 16:49:54 +0530

    Update server.js removed local url

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const UsersRoute = require('./router/usersRout')
-// mongoose.connect('mongodb://127.0.0.1:27017/online_auction', { useNewUrlParser: true, useUnifiedTopology: true });
-
 const uri = "mongodb+srv://sbraviprakasha:HqfyL5v0tG7O8dyD@cluster0.tktppdm.mongodb.net/online_auction?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
